35d6cf3d23f519bdca0b323b081b769620396d4f
Igor_Zhidkov <stork32@mail.ru>
2023-05-29 18:03:20 +0300

    last1

--- Itog/src/App.java
+++ Itog/src/App.java
@@ -1,5 +1,17 @@
+import Controller.CalcController;
+import Model.ComplexCalcModel;
+import Model.ComplexNumber;
+import Model.TxtLogger;
+import Model.iCalcModel;
+import Model.iLogger;
+import View.DefaultCalcView;
+import View.iCalcView;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        iCalcModel<ComplexNumber> model = new ComplexCalcModel();
+        iCalcView<ComplexNumber> view = new DefaultCalcView();
+        iLogger<ComplexNumber> logger = new TxtLogger("Less4x");
+        CalcController<ComplexNumber> controller = new CalcController<ComplexNumber>(model, view, logger);
     }
 }
